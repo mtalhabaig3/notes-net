cf9897da64af94da5ed3ec9d350a0f31a678e058
mtalhabaig3 <mtalhabaig3@gmail.com>
2022-10-10 23:21:40 +0500

    Added a route to authenticate a user

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -9,11 +9,13 @@ var bcrypt = require("bcryptjs");
 const JWT_SECRET = "Assalam u Alaikum";
 
 router.post(
-  "/CreateUser",
+  "/createUser",
   [
-    body("name").isLength({ min: 3 }),
-    body("email").isEmail(),
-    body("password").isLength({ min: 5 }),
+    body("name", "name must be atleast 3 characters").isLength({ min: 3 }),
+    body("email", "Enter valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters long").isLength({
+      min: 5,
+    }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -34,6 +36,44 @@ router.post(
         email: req.body.email,
         password: secPass,
       });
+      const data = {
+        id: user.id,
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json(authtoken);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+);
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
       const data = {
         id: user.id,
       };
